4c8600bdc36fc147573616a5cdb6d21613b660d2
Isham Araia <ishamaraia@gmail.com>
2025-12-23 15:39:53 +0100

    setting up minikube for test env

--- hack/setup.sh
+++ hack/setup.sh
@@ -3,10 +3,19 @@
 
 set -e
 
+# NOTE: before running this script install crio, kubelet, docker and run minikube start --driver=docker --cni=calico
+echo "NOTE: before running this script install crio, kubelet, docker and run minikube start --driver=docker --cni=calico"
+echo "---"
+echo "press <enter> to continue"
+read pause_exec
+
 echo "Creating directory structure & certs.."
 mkdir -p /etc/kubernetes/
 mkdir -p /var/lib/kubelet/pki
 mkdir -p /var/lib/kubelet/certs
+mkdir -p /custom-node/etc/cni/
+mkdir -p /custom-node/opt/cni/
+
 openssl genrsa -out /var/lib/kubelet/pki/custom-node.key 2048
 openssl req -new -key /var/lib/kubelet/pki/custom-node.key -out  /var/lib/kubelet/pki/custom-node.csr -subj "/O=system:nodes/CN=system:node:custom-node"
 openssl x509 -req -in /var/lib/kubelet/pki/custom-node.csr -CA /home/ish/.minikube/ca.crt -CAkey /home/ish/.minikube/ca.key -CAcreateserial -out /var/lib/kubelet/pki/custom-node.crt -days 365 -extensions v3_req
@@ -15,6 +24,8 @@ echo "Exporting configs..."
 docker cp minikube:/etc/kubernetes/kubelet.conf /etc/kubernetes/kubelet.conf
 docker cp -L minikube:/var/lib/minikube/certs/ca.crt /var/lib/kubelet/certs/ca.crt
 docker cp minikube:/var/lib/kubelet/config.yaml /var/lib/kubelet/config.yaml
+docker cp minikube:opt/cni/bin /custom-node/opt/cni/
+docker cp minikube:/etc/cni/net.d /custom-node/etc/cni/
 
 echo "Customizing exported config..."
 sed -i 's#unix:///var/run/cri-dockerd.sock#unix:///run/crio/crio.sock#g' /var/lib/kubelet/config.yaml
@@ -24,6 +35,7 @@ sed -i 's#client-key: /var/lib/kubelet/pki/kubelet-client-current.pem#client-key
 
 echo "Setting up kubelet systemd unit..."
 cp kubelet.service /usr/lib/systemd/system/kubelet.service
+cp crio.conf /etc/crio/crio.conf.d/10-crio.conf
 systemctl daemon-reload
 
 echo "Restarting crio..."
@@ -31,3 +43,6 @@ systemctl restart crio
 
 echo "Restarting kubelet..."
 systemctl restart kubelet
+
+echo "Labeling node.."
+kubectl --kubeconfig=/home/ish/.kube/config label no custom-node kubernetes.io/role=worker
